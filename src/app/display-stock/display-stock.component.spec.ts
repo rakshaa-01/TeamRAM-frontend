5170944214ac67cadc31029b814881052fd68865
Rakshaa M <divya123this@gmail.com>
2023-08-18 07:59:23 +0000

    Angular-testing

--- src/app/display-stock/display-stock.component.spec.ts
+++ src/app/display-stock/display-stock.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { DisplayStockComponent } from './display-stock.component';
 
@@ -8,7 +10,8 @@ describe('DisplayStockComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DisplayStockComponent]
+      declarations: [DisplayStockComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule]
     });
     fixture = TestBed.createComponent(DisplayStockComponent);
     component = fixture.componentInstance;
